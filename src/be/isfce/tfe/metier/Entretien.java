b16982d3e5150d20a92b1aa09e71e3be262f2cf2
chezyema <chezyema@hotmail.com>
2014-02-22 15:28:03 +0100

    Initial commit

---
+++ src/be/isfce/tfe/metier/Entretien.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package be.isfce.tfe.metier;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+import java.util.Date;
+
+/**
+ *
+ * @author yema
+ */
+@DatabaseTable(tableName="Entretien")
+public class Entretien {
+    @DatabaseField(columnName="id",id=true)
+    private int id;
+    private  String description;
+    private  int kmEntretienFait;
+    private Date dateEntretien; 
+
+    public Entretien() {
+    }
+
+    public Entretien(int id, String description, int kmEntretienFait, Date dateEntretien) {
+        this.id = id;
+        this.description = description;
+        this.kmEntretienFait = kmEntretienFait;
+        this.dateEntretien = dateEntretien;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getKmEntretienFait() {
+        return kmEntretienFait;
+    }
+
+    public void setKmEntretienFait(int kmEntretienFait) {
+        this.kmEntretienFait = kmEntretienFait;
+    }
+
+    public Date getDateEntretien() {
+        return dateEntretien;
+    }
+
+    public void setDateEntretien(Date dateEntretien) {
+        this.dateEntretien = dateEntretien;
+    }
+    
+    
+    
+}
